b1da5ecbec500cc58a39540012ca8365d50418d9
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-18 15:33:25 +0000

    Add html file in contactapp and give style in css file and add contactapp in home page

---
+++ contact/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """ Form for contact """
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your email'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your message'}))
--- home/views.py
+++ home/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render
 from menu.models import Meals, Category
-#from aboutus.models import AboutUs, Why_Choose_Us, Chef
-#from contact.forms import ContactForm
+from aboutus.models import AboutUs, Why_Choose_Us, Chef
+from contact.forms import ContactForm
 from django.contrib import messages
-# from reservation.models import Reservation
 #from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 #@login_required(login_url='/reserve_table/login')
 def home(request):
-   #about = AboutUs.objects.last()
-    #why_choose_us = Why_Choose_Us.objects.all()
+    about = AboutUs.objects.last()
+    why_choose_us = Why_Choose_Us.objects.all()
     meals = Meals.objects.all()
     categories = Category.objects.all()
     meal_list = Meals.objects.all()
-    #chef = Chef.objects.all()
-    #form = ContactForm()
+    chef = Chef.objects.all()
+    form = ContactForm()
 
     context = {
-        #'about' : about,
-        #'why_choose_us' : why_choose_us,
+        'about' : about,
+        'why_choose_us' : why_choose_us,
         'meals': meals,
         'categories': categories,
         'meal_list': meal_list,
-        #'chef' : chef,
-        #'form' : form,
+        'chef' : chef,
+        'form' : form,
 
     }
 
