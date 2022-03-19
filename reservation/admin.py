4e34a660eb2bdcfb9745ffb12dc626b6f900e2c8
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-19 16:40:17 +0000

    create model for Customer in models.py

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Customer, Table, Reservation
 
 # Register your models here.
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+
+
+# @admin.register(Table)
+# class TableAdmin(admin.ModelAdmin):
+#     list_display = ('table_name', 'max_no_people')
+
+
+# @admin.register(Reservation)
+# class ReservationsAdmim(admin.ModelAdmin):
+#     list_filter = ('persons', 'status',)
+#     list_display = ('customer', 'persons', 'status',
+#                     'table', 'date', 'time')
+
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+time_choices = (
+    ("07:00", "07:00"),
+    ("08:00", "08:00"),
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+    ("18:00", "18:00"),
+    ("19:00", "19:00"),
+    ("20:00", "20:00"),
+    ("21:00", "21:00"),
+    )
+
+
+
+
+class Customer(models.Model):
+
+    name = models.CharField(max_length=50, null=True)
+    email = models.EmailField(default='')
+    phone = models.CharField(max_length=13, default='')
+    owner = models.IntegerField(blank=False, default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+
+    table_name = models.CharField(max_length=20, default="New table", unique=True)
+    max_no_people = models.IntegerField()
+
+    def __str__(self):
+        return self.table_name
+
+
