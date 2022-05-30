8cdb8ae60fd1dc8c59bd3e9eeb11a39387b11bdf
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-05-30 15:16:03 +0000

    Added email template in contact

--- contact/views.py
+++ contact/views.py
@@ -5,10 +5,11 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.conf import settings
 from .forms import ContactForm
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 def send_email(request):
-
     """ Email sending form """
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -18,14 +19,27 @@ def send_email(request):
             message = form.cleaned_data['message']
             recipient_list = [settings.EMAIL_HOST_USER]
             send_mail(
-
-
                 'From: ' + name,
-                message,
-                email,
+                'Message: ' + message,
+                'To' + email,
                 recipient_list,
+                fail_silently=False,
+            )
 
-                )
+            email = request.POST.get('email')
+            html_message = render_to_string(
+                'contact/contacts-mail.html', {'name': request.user.first_name})
+            plain_message = strip_tags(html_message)
+
+            send_mail(
+                'From: Mochi Restaurant',
+                plain_message,
+                'mochi_restaurant@site.com',
+                [email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+
             messages.add_message(request, messages.SUCCESS, f" {name} ")
 
         return HttpResponseRedirect('/contact/')
