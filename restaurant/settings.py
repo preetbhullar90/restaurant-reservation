97271353d6ac6fe684db1dd652089efaecdf36fb
preetbhullar90 <gurpreet.singhb90@gmail.com>
2023-01-10 21:42:13 +0000

    new commit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -151,6 +151,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
